f812d48a27db3e291cfbd7beda008f608cc72aca
Alejandro <alejandro.uria@uam.es>
2020-12-15 20:42:05 +0100

    Adapted real-space wf module to classes

--- lib/libwavefunction.hpp
+++ lib/libwavefunction.hpp
@@ -2,10 +2,10 @@
 #include <armadillo>
 #include <complex>
 
-double sWaveFunction(const arma::vec&, const arma::vec&);
-arma::cx_mat collapseTBcoefs(const arma::cx_mat&);
-arma::cx_mat RScoefficientCalc(const arma::cx_vec&, 
-                               const arma::vec&, int, int, int);
-double electronicDensity(const arma::vec&, const arma::cx_mat&, int, 
-                         int, const arma::vec&);
-double atomCoefficientSquared(int, int, int, const arma::cx_mat&, const arma::vec&);
+#include "Zigzag.hpp"
+#include "Exciton.hpp"
+
+arma::cx_mat RScoefficientCalc(Exciton&, const arma::cx_vec&, 
+                               int);
+double atomCoefficientSquared(int, int, int, const arma::cx_mat&, 
+                              Exciton&);
