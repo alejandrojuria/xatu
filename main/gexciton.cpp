bd508387bd852582faea769b7b08fde19c96156c
Alejandro-dell <alejandro.uria@uam.es>
2022-04-27 15:31:40 +0200

    Better formated output

--- main/gexciton.cpp
+++ main/gexciton.cpp
@@ -9,6 +9,9 @@
 
 #include "GExciton.hpp"
 #include "System.hpp"
+#include "Crystal.hpp"
+#include "Result.hpp"
+#include "utils.hpp"
 
 #ifndef constants
 #define PI 3.141592653589793
@@ -29,146 +32,107 @@ int main(int argc, char* argv[]){
     
     int nbands = 1;
     int nrmbands = 0;
-    int Ncell = 21;
+    int ncell = 20;
     double filling = 1./2;
     arma::rowvec Q = {0., 0., 0.};
-    std::string modelfile = argv[1];
-    
-    bool useCenteredMeshK = false;
+    arma::rowvec parameters = {1., 1., 10};
+    std::string modelfile = argv[1];    
 
     // ----------------- Model parameters & Output --------------------
-
+    bool writeEigvals = false;
     std::string filename = "eigval.out";
     FILE* textfile_en = fopen(filename.c_str(), "w");
-    bool writeEigvals = false;
 
-    std::string filename_wf = "k_wf.out";
-    FILE* textfile_wf = fopen(filename_wf.c_str(), "w");
-    bool printWF = true;
+    bool writeStates = true;
+    std::string filename_st = "states.out";
+    FILE* textfile_st = fopen(filename_st.c_str(), "w");
+        
+    bool writeWF = false;
+    std::string filename_kwf = "kwf.out";
+    FILE* textfile_kwf = fopen(filename_kwf.c_str(), "w");
 
-    bool calculateSpin = false;
-    std::string filename_spin = "spin.out";
-    FILE* textfile_spin = fopen(filename_spin.c_str(), "w");
-    bool writeSpin = false;
+    bool writeRSWF = false;
+    std::string filename_rswf = "rswf.out";
+    FILE* textfile_rswf = fopen(filename_rswf.c_str(), "w");
 
-    // ----------------------- Main body -----------------------
+    // -------------------------- Main body ---------------------------
 
-    fprintf(textfile_en, "#k points\tTotal energy\tTB energy\tV energy (eV)\n");
-    cx_mat eigvecX;
-    vec eigvalX;
-
-    cout << "##############################################################################" << endl;
-    cout << "--------------------------------- Parameters ---------------------------------" << endl;
-    cout << "N. cells: " << Ncell << endl;
+    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << "|                                  Parameters                               |" << endl;
+    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << "N. cells: " << ncell*ncell << endl;
     cout << "#bands: " << nbands << endl;
     cout << "#removed bands: " << nrmbands << "\n" << endl;
 
-    GExciton bulkExciton = GExciton(modelfile, Ncell, Q, nbands, nrmbands, filling, true);
-    int nk = bulkExciton.nk;
-    Ncell = bulkExciton.Ncell;
-    
-    /*double norm = arma::norm(bulkExciton.bravais_lattice.row(0)) * Ncell/2.5;
-    cout << norm << endl;
-    arma::mat combinations = bulkExciton.generate_combinations_gamma(Ncell, 2);
-    for (int i = 0; i < combinations.n_rows; i++){
-        arma::rowvec lattice_vector = combinations.row(i)(0)*bulkExciton.bravais_lattice.row(0) + 
-        combinations.row(i)(1)*bulkExciton.bravais_lattice.row(1);
-        if (arma::norm(lattice_vector) < norm + 1E-5){
-            fprintf(textfile_wf, "%10.7lf\t%10.7lf\t%d\n", 
-                lattice_vector(0), lattice_vector(1), 1);
-        }
-        else{
-            fprintf(textfile_wf, "%10.7lf\t%10.7lf\t%d\n", 
-                lattice_vector(0), lattice_vector(1), 0);
-        }
-    }*/
-    
-    
-    /*if(useCenteredMeshK){
-        double norm = arma::norm(bulkExciton.reciprocalLattice.row(0));
-        arma::rowvec K = norm/sqrt(3)*(bulkExciton.reciprocalLattice.row(0)/2. -
-                                bulkExciton.reciprocalLattice.row(1)/2.)/arma::norm(
-                                bulkExciton.reciprocalLattice.row(0)/2. -
-                                bulkExciton.reciprocalLattice.row(1)/2.);
-                                
-        int reductionFactor = 4;
-        for(int i = 0; i < nk; i++){
-            bulkExciton.kpoints.row(i) /= reductionFactor;
-            bulkExciton.kpoints.row(i) += K;
-        };
-        bulkExciton.Ncell *= reductionFactor;
-        bulkExciton.reinitializeInternals();
-        cout << bulkExciton.Ncell << ", " << bulkExciton.nk << endl;
-        bulkExciton.nk = pow(bulkExciton.Ncell, bulkExciton.ndim);
-    }
-    */
+    GExciton bulkExciton = GExciton(modelfile, ncell, nbands, nrmbands, parameters);
+    //bulkExciton.setGauge("atomic");
 
-    cout << "Valence bands:" << bulkExciton.valenceBands << endl;
-    cout << "Conduction bands:" << bulkExciton.conductionBands << endl;
-    bulkExciton.BShamiltonian();
-    arma::eig_sym(eigvalX, eigvecX, bulkExciton.HBS);
+    cout << "Valence bands:\n" << bulkExciton.valenceBands << endl;
+    cout << "Conduction bands:\n" << bulkExciton.conductionBands << endl;
+    cout << "Gauge used: " << bulkExciton.gauge << "\n" << endl;
+    
+    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << "|                                Initialization                             |" << endl;
+    cout << "-----------------------------------------------------------------------------" << endl;
 
-    cout << "#----------------------- Energy -----------------------" << endl;
-    cout << "Fundamental state energy is: " << eigvalX(0) << endl;
-    cout << "Second state energy is: " << eigvalX(1) << endl;
-    cout << "Third state energy is: " << eigvalX(2) << endl;
-    cout << "Fourth state energy is: " << eigvalX(3) << endl;
-    cout << "Delta E: " << eigvalX(1) - eigvalX(0) << endl;
-    vec energies = bulkExciton.computeEnergies(eigvecX.col(0));
-    cout << "Kinectic energy: "  << energies(0) << endl;
-    cout << "Potential energy: " << energies(1) << "\n" << endl;
+    bulkExciton.brillouinZoneMesh(ncell);
+    bulkExciton.initializeHamiltonian();
+    bulkExciton.BShamiltonian({}, false);
+    auto results = bulkExciton.diagonalize();
+
+    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << "|                                    Results                                |" << endl;
+    cout << "-----------------------------------------------------------------------------" << endl;
+
+    cout << "#--------------------------------- Ground state -----------------------------" << endl;
+    cout << "Fundamental state energy is: " << results.eigval(0) << endl;
+    cout << "Binding energy: " << results.bindingEnergy(0) << endl;
+    cout << "Kinectic energy: "  << results.kineticEnergy(0) << endl;
+    cout << "Potential energy: " << results.potentialEnergy(0) << "\n" << endl;
+    cout << "#-------------------------------- Excited states ----------------------------" << endl;
+    cout << "Second state energy is: " << results.eigval(1) << endl;
+    cout << "Third state energy is: " << results.eigval(2) << endl;
+    cout << "Fourth state energy is: " << results.eigval(3) << endl;
+    cout << "Fifth state energy is: " << results.eigval(4) << endl;
+    cout << "Sixth state energy is: " << results.eigval(5) << endl;
+    cout << "Seventh state energy is: " << results.eigval(6) << endl;
+    cout << "Eigth state energy is: " << results.eigval(7) << endl;
+    cout << "Delta E: " << results.eigval(0) - results.eigval(1) << endl;
+
+    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << "|                                    Output                                 |" << endl;
+    cout << "-----------------------------------------------------------------------------" << endl;
 
     if(writeEigvals){
-        //writeEigenvaluesToFile(textfile, eigvalX, Q);
-        fprintf(textfile_en, "%d\t", Ncell);
-        for(int i = 0; i < eigvalX.n_elem; i++){
-            fprintf(textfile_en, "%10.7lf\t", eigvalX(i));
-        }
-        fprintf(textfile_en, "\n");
-    };
+        std::cout << "Writing eigvals to file: " << filename << std::endl;
+        fprintf(textfile_en, "%d\n", ncell);
+        results.writeEigenvalues(textfile_en, 7);
+    }
 
-    //cx_mat states = bulkExciton.fixDegeneracy(eigvecX.col(0), eigvecX.col(1), 20);
-    //cx_vec state = states.col(0);
-
-    if(printWF){
-        fprintf(textfile_wf, "kx\tky\tkz\tProb.\n");
-        cx_vec state = eigvecX.col(0);
-        //cx_vec state = (eigvecX.col(0) + eigvecX.col(1))/sqrt(2);
-        cx_vec deg_state = eigvecX.col(0);
-        int nbandsCombinations = bulkExciton.conductionBands.n_elem*
-                                bulkExciton.valenceBands.n_elem;
-        for (int i = 0; i < bulkExciton.kpoints.n_rows; i++){
-            double coef = 0;
-            for(int nband = 0; nband < nbandsCombinations; nband++){
-                coef += abs(state(nbandsCombinations*i + nband))*
-                        abs(state(nbandsCombinations*i + nband));
-                //coef += abs(eigvecX.col(state + 1)(nbandsCombinations*i + nband))*abs(eigvecX.col(state + 1)(nbands2*i + nband));
-            };
-            coef /= arma::norm(bulkExciton.kpoints.row(1) - bulkExciton.kpoints.row(0)); // L2 norm instead of l2
-            fprintf(textfile_wf, "%11.8lf\t%11.8lf\t%11.8lf\t%11.8lf\n", 
-                    bulkExciton.kpoints.row(i)(0), bulkExciton.kpoints.row(i)(1), 
-                    bulkExciton.kpoints.row(i)(2), coef);
-        };
-        fprintf(textfile_wf, "#\n");
-    };
-
-    cx_vec spin;
-    if(calculateSpin){
-        spin = bulkExciton.spinX(eigvecX.col(0));
-        cout << "#------------------------ Spin ------------------------" << endl;
-        cout << "Hole Sz is: " << spin(0) << endl;
-        cout << "Electron Sz is: " << spin(1) << endl;
-        cout << "Total spin is: " << spin(2) << "\n" << endl;
-                
-        if(writeSpin){
-            fprintf(textfile_spin, "%d\t%13.10lf\n", nk, real(spin(2)));
-        };
-    };
+    if(writeStates){
+        std::cout << "Writing states to file: " << filename_st << std::endl;
+        results.writeStates(textfile_st);
+    }
     
+    if(writeWF){
+        std::cout << "Writing k w.f. to file: " << filename_kwf << std::endl;
+        int nstates = 8;
+        for(int stateindex = 0; stateindex < nstates; stateindex++){
+            results.writeExtendedReciprocalAmplitude(stateindex, textfile_kwf);        
+        }
+    }
+
+    if(writeRSWF){
+        std::cout << "Writing real space w.f. to file: " << filename_rswf << std::endl;
+        int holeIndex = 0;
+        arma::rowvec holeCell = {0., 0., 0.};
+        results.writeRealspaceAmplitude(0, holeIndex, holeCell, textfile_rswf);
+    }
 
     fclose(textfile_en);
-    fclose(textfile_wf);
-    fclose(textfile_spin);
+    fclose(textfile_st);
+    fclose(textfile_kwf);
+    fclose(textfile_rswf);
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
