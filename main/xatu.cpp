b770d5042a9778842df668e2259704d93eca6762
Alejandro <alejandro.uria@uam.es>
2023-03-21 20:02:21 +0100

    Fixed flag in xatu

--- main/xatu.cpp
+++ main/xatu.cpp
@@ -45,7 +45,7 @@ int main(int argc, char* argv[]){
     std::vector<std::string> methods = {"diag", "davidson"};
     TCLAP::ValuesConstraint<std::string> allowedMethods(methods);
     TCLAP::ValueArg<std::string> methodArg("m", "method", "Method to solve the Bethe-Salpeter equation.", false, "diag", &allowedMethods, cmd);
-    TCLAP::ValueArg<std::string> bandsArg("b", "bands", "Computes the bands of the system on the specified kpoints.", false, "kpoints.txt", "Filename");
+    TCLAP::ValueArg<std::string> bandsArg("b", "bands", "Computes the bands of the system on the specified kpoints.", false, "kpoints.txt", "Filename", cmd);
     
     TCLAP::UnlabeledValueArg<std::string> systemArg("systemfile", "System file", true, "system.txt", "filename", cmd);
     TCLAP::UnlabeledValueArg<std::string> excitonArg("excitonfile", "Exciton file", false, "exciton.txt", "filename", cmd);
@@ -73,7 +73,6 @@ int main(int argc, char* argv[]){
 
     std::string systemfile  = systemArg.getValue();
     std::string excitonfile = excitonArg.getValue();
-
     std::string kpointsfile = bandsArg.getValue();
 
     // Init. configurations
@@ -93,37 +92,17 @@ int main(int argc, char* argv[]){
         xatu::System system = xatu::System(*systemConfig);
         system.solveBands(kpointsfile, triangular);
 
+        systemConfig.reset();
+
         return 0;
     }
     else{
         if (!excitonArg.isSet()){
             throw std::invalid_argument("Must provide exciton file.");
         }
+
         excitonConfig.reset(new xatu::ExcitonConfiguration(excitonfile));
     }
-
-    std::string output = systemConfig->systemInfo.name;
-
-    // --------------------------- Output ---------------------------
-    bool writeEigvals = energyArg.isSet();
-    std::string filename_en = output + ".eigval";
-    FILE* textfile_en = fopen(filename_en.c_str(), "w");
-
-    bool writeStates = eigenstatesArg.isSet();
-    std::string filename_st = output + ".states";
-    FILE* textfile_st = fopen(filename_st.c_str(), "w");
-        
-    bool writeWF = reciprocalArg.isSet();
-    std::string filename_kwf = output + ".kwf";
-    FILE* textfile_kwf = fopen(filename_kwf.c_str(), "w");
-
-    bool writeRSWF = realspaceArg.isSet();
-    std::string filename_rswf = output + ".rswf";
-    FILE* textfile_rswf = fopen(filename_rswf.c_str(), "w");
-
-    bool writeAbs = absorptionArg.isSet();
-    std::string filename_abs = output + ".abs";
-    FILE* textfile_abs = fopen(filename_abs.c_str(), "w");
 
     // -------------------------- Main body ---------------------------
 
@@ -158,25 +137,50 @@ int main(int argc, char* argv[]){
     cout << "|                                    Output                                 |" << endl;
     cout << "+---------------------------------------------------------------------------+" << endl;
 
+    std::string output = systemConfig->systemInfo.name;
+
+    // --------------------------- Output ---------------------------
+    bool writeEigvals = energyArg.isSet();
     if(writeEigvals){
+        std::string filename_en = output + ".eigval";
+        FILE* textfile_en = fopen(filename_en.c_str(), "w");
+
         std::cout << "Writing eigvals to file: " << filename_en << std::endl;
         fprintf(textfile_en, "%d\n", excitonConfig->excitonInfo.ncell);
         results.writeEigenvalues(textfile_en, nstates);
-    }
 
-    if(writeStates){
-        std::cout << "Writing states to file: " << filename_st << std::endl;
-        results.writeStates(textfile_st, nstates);
+        fclose(textfile_en);
     }
     
+    bool writeStates = eigenstatesArg.isSet();
+    if(writeStates){
+        std::string filename_st = output + ".states";
+        FILE* textfile_st = fopen(filename_st.c_str(), "w");
+
+        std::cout << "Writing states to file: " << filename_st << std::endl;
+        results.writeStates(textfile_st, nstates);
+
+        fclose(textfile_st);
+    }
+    
+    bool writeWF = reciprocalArg.isSet();
     if(writeWF){
+        std::string filename_kwf = output + ".kwf";
+        FILE* textfile_kwf = fopen(filename_kwf.c_str(), "w");
+
         std::cout << "Writing k w.f. to file: " << filename_kwf << std::endl;
         for(int stateindex = 0; stateindex < nstates; stateindex++){
             results.writeExtendedReciprocalAmplitude(stateindex, textfile_kwf);        
         }
-    }
 
+        fclose(textfile_kwf);
+    }
+    
+    bool writeRSWF = realspaceArg.isSet();
     if(writeRSWF){
+        std::string filename_rswf = output + ".rswf";
+        FILE* textfile_rswf = fopen(filename_rswf.c_str(), "w");
+
         arma::uvec statesToWrite = arma::regspace<arma::uvec>(0, nstates - 1);
         std::cout << "Writing real space w.f. to file: " << filename_rswf << std::endl;
         arma::rowvec holeCell = {0., 0., 0.};
@@ -185,18 +189,20 @@ int main(int argc, char* argv[]){
             std::cout << "Writing state " << i + 1 << " out of " << statesToWrite.n_elem << std::endl;
             results.writeRealspaceAmplitude(statesToWrite(i), holeIndex, holeCell, textfile_rswf, ncellsRSWF);
         }
+
+        fclose(textfile_rswf);
     }
 
+    bool writeAbs = absorptionArg.isSet();
     if(writeAbs){
+        std::string filename_abs = output + ".abs";
+        FILE* textfile_abs = fopen(filename_abs.c_str(), "w");
+
         std::cout << "Writing absorption spectrum fo file... " << std::endl;
         results.writeAbsorptionSpectrum();
-    }
 
-    fclose(textfile_en);
-    fclose(textfile_st);
-    fclose(textfile_kwf);
-    fclose(textfile_rswf);
-    fclose(textfile_abs);
+        fclose(textfile_abs);
+    }
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
