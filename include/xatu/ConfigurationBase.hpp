ceb13d96034c19162d682d82d8727c1477109e29
Alejandro <alejandro.uria@uam.es>
2023-07-01 18:44:26 +0200

    Added docs to ConfigurationBase

--- include/xatu/ConfigurationBase.hpp
+++ include/xatu/ConfigurationBase.hpp
@@ -7,14 +7,27 @@
 
 namespace xatu {
 
+/**
+ * The ConfigurationBase class provides functionality to parse general configuration files. 
+ * Its purpose is not be instantiated by itself, but to serve as a base class for more specialized
+ * configuration classes.
+ */
 class ConfigurationBase {
 
     public:
+        // Name of configuration file to be parsed.
         std::string filename;
+        // Raw text read from the file without any parsing.
         std::string text;
+        // List of arguments found in configuration file.
         std::vector<std::string> foundArguments;
+        // List of arguments expected to be present in configuration file.
         std::vector<std::string> expectedArguments;
+        // Parsed information of file already fully structured.
         struct configuration;
+        // Semi-structured contents of the file. Each key of the dictionary corresponds
+        // to one argument, while its value is the raw text corresponding to that argument,
+        // which is to be processed further.
         std::map<std::string, std::vector<std::string>> contents;
 
     protected:
@@ -30,6 +43,12 @@ class ConfigurationBase {
         virtual void parseContent() = 0;
         void checkArguments();
 
+        /**
+         * Template method to parse a line made of some values of type T into a vector
+         * with those values.
+         * @param line String to be parsed into a vector.
+         * @returns Vector of values T.
+         */
         template<typename T>
         std::vector<T> parseLine(const std::string& line){
             std::vector<T> values;
@@ -43,6 +62,11 @@ class ConfigurationBase {
 
         std::string standarizeLine(std::string&);
 
+        /**
+         * Template method to parse one line containing only one value of a type T.
+         * @param line String to parse.
+         * @returns Value T.
+         */
         template<typename T>
         T parseScalar(std::string& line){
             T value;
@@ -51,6 +75,10 @@ class ConfigurationBase {
             return value;
         }
 
+        /**
+         * Template method to print a vector of type T to screen.
+         * @param v Vector to print.
+         */
         template<typename T>
         void printVector(std::vector<T>& v){
             for (auto i = v.begin(); i != v.end(); i++){
