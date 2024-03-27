cebdec6f28a4f6ba201250fb4c79714076d19ba1
alejandrojuria <alejandrojuria@gmail.com>
2024-03-27 11:14:00 +0100

    Templated method printEnergies and added checkIfTriangular

--- include/xatu/utils.hpp
+++ include/xatu/utils.hpp
@@ -10,22 +10,52 @@
 
 namespace xatu {
 
+
 /* Output */
 void writeVectorToFile(arma::vec, FILE*);
 void writeVectorToFile(arma::rowvec, FILE*);
 void writeVectorsToFile(const arma::mat&, FILE*, std::string mode = "row");
 std::vector<std::vector<double>>  detectDegeneracies(const arma::vec&, int, int);
-void printEnergies(Result, int n = 8, int precision = 6);
 void printHeader();
 
 /* Input */
 arma::vec readVectorFromFile(std::string);
 
-
 /* Routines for DoS calculation */
 std::complex<double> rGreenF(double, double, double);
 double densityOfStates(double, double, const arma::mat&);
 void writeDensityOfStates(const arma::mat&, double, FILE*);
+
+/* Matrix properties */
+bool checkIfTriangular(const arma::cx_mat&);
+
+/** Routine to pretty print the eigenenergies from the BSE calculation. Computes number of degenerate states 
+* corresponding to each energy level. 
+* @param results Unique pointer to Result object
+* @param n Number of energies 
+* @param precision Number of decimals (sets degeneracy threshold)
+**/
+template<typename T>
+void printEnergies(const std::unique_ptr<T>& results, int n = 8, int precision = 6){
+
+    // Print header
+    printf("+---------------+-----------------------------+-----------------------------+\n");
+    printf("|       N       |          Eigval (eV)        |          Degeneracy         |\n");
+    printf("+---------------+-----------------------------+-----------------------------+\n");
+
+    std::vector<std::vector<double>> pairs = detectDegeneracies(results->eigval, n, precision);
+    int it = 1;
+
+    for(auto pair : pairs){
+        double energy  = pair[0];
+        int degeneracy = (int)pair[1];
+
+        printf("|%15d|%29.*lf|%29d|\n", it, precision, energy, degeneracy);
+        printf("+---------------+-----------------------------+-----------------------------+\n");
+
+        it++;
+    }
+}
 
 /* Routines for tests */
 template<typename T>
