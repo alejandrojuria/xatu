8edb6ea46484b4e0b8a4751e239890c981189fda
GarciaBlazquez <garcia.blazquez.antonio@hotmail.com>
2024-05-31 09:33:49 +0200

    davidson: a couple of int->int64_t conversions to make it safer

--- src/davidson.cpp
+++ src/davidson.cpp
@@ -6,11 +6,11 @@ void davidson_method(
     arma::vec& eigval, 
     arma::cx_mat& eigvec, 
     const arma::cx_mat& mat, 
-    int neigval, 
-    double tol){
+    const int64_t neigval, 
+    const double tol){
 
-    int max_iterations = mat.n_rows/2;
-    int k = neigval * 2;
+    int64_t max_iterations = mat.n_rows/2;
+    int64_t k = neigval * 2;
         
     if(!mat.is_hermitian()){
         throw std::invalid_argument("davidson_method: provided matrix must be hermitian");
@@ -21,7 +21,7 @@ void davidson_method(
     arma::vec aux_eigval = arma::ones(neigval);
     arma::cx_mat Q, R;
 
-    for(int i = 0; i < max_iterations; i++){
+    for(int64_t i = 0; i < max_iterations; i++){
 
         // QR decomposition
         Q.clear();
@@ -33,7 +33,7 @@ void davidson_method(
 
         // Add new eigvec to guess
         eigvec = Q.cols(0, (i + 1)*k - 1) * eigvec.cols(0, k - 1);
-        for (int j = 0; j < k; j++){
+        for (int64_t j = 0; j < k; j++){
             arma::cx_mat w = (mat - eigval(j)*arma::eye<arma::cx_mat>(mat.n_rows, mat.n_cols))*eigvec.col(j);
             arma::cx_vec normalized_w = w/(eigval(j) - mat(j, j));
             Q.insert_cols(Q.n_cols, normalized_w);
@@ -58,6 +58,6 @@ void davidson_method(
     eigval = eigval.subvec(0, neigval - 1);
     arma::cout << eigvec.n_rows << arma::endl;
     arma::cout << eigvec.n_cols << arma::endl;
-};
+}
 
 }
